7098c1d468959930b1a959dfc36146749eb9dea2
Arnas Liesis <arnutislie12@gmail.com>
2024-08-10 20:40:59 -0400

    the telegram bot can now hold a conversation and you can clear the history when texting it

--- main.py
+++ main.py
@@ -21,20 +21,27 @@ def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
 @bot.message_handler(commands=['labas'])
-def send_sveikas(message)
-	bot.reply_to(message, "Sveikas seni, kaip tu?")
+def send_sveikas(message):
+	bot.reply_to(message, "Sveikas seni, ka tu?")
+
+@bot.message_handler(commands=['clear'])
+def clear_chat(message):
+	chat_history.clear()
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	print("Sending to mistral ......")
+	add_message_to_history("user", str(message.text))
+	latest_conversation = get_history()
 	mistral_response = client.chat(
 		model="mistral-small-latest",
-		messages=[ChatMessage(role="user", content=str(message.text))]
+		messages=latest_conversation
 	)
+	add_message_to_history("assistant", str(mistral_response.choices[0].message.content))
 	bot.reply_to(message, mistral_response.choices[0].message.content)
 
-def add_user_message_to_history(role, message):
-	chat_history.append(ChatMessage(role=role, content=message))
+def add_message_to_history(role, message):
+	chat_history.append({"role":role, "content":message})
 	return
 
 def get_history():
